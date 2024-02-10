d49499e8a0ddd30e172b24e00dbcd589221e4f99
Arif Ramdani <108125940+shelllbyyyyyy@users.noreply.github.com>
2024-02-10 17:13:43 +0700

    feat(auth): implement supabase guard on server

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
+import { AppModule } from '@/app.module';
+import { HttpExceptionFilter } from '@/http-exception.filter';
+
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://mabar-fun-web.vercel.app'],
+  });
+
+  const httpAdapterHost = app.get(HttpAdapterHost);
+
+  app.useGlobalFilters(new HttpExceptionFilter(httpAdapterHost));
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   await app.listen(4000);
 }
 bootstrap();
