5d1a024cc9ca1dc727facb9ae1253765e07c984c
Arif Ramdani <108125940+shelllbyyyyyy@users.noreply.github.com>
2024-02-12 11:35:49 +0700

    feat(profiles): add initial edit profile

---
+++ apps/server/src/core/profile/profile.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+
+import { PrismaService } from '@/lib/prisma.service';
+import { SupabaseService } from '@/lib/supabase.service';
+import { ProfileController } from './profile.controller';
+import { ProfileService } from './profile.service';
+
+@Module({
+  imports: [],
+  controllers: [ProfileController],
+  providers: [ProfileService, PrismaService, SupabaseService],
+})
+export class ProfileModule {}
---
+++ apps/server/src/core/profile/profile.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@shelby/db';
+import { EditProfileDTO } from '@shelby/dto';
+
+import { PrismaService } from '@/lib/prisma.service';
+import { SupabaseService } from '@/lib/supabase.service';
+import { SupabaseBucket } from '@/types/supabase-bucket.enum';
+
+@Injectable()
+export class ProfileService {
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly supabaseService: SupabaseService,
+  ) {}
+
+  public async getProfile(id: string) {
+    const profile = await this.prismaService.profiles.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return profile;
+  }
+
+  public async editProfile(
+    id: string,
+    editProfileDTO: EditProfileDTO,
+    profilePictureFile?: Express.Multer.File,
+  ) {
+    const editProfilePayload: Prisma.ProfilesUpdateInput = {
+      ...editProfileDTO,
+    };
+
+    if (profilePictureFile) {
+      const fileUrl = await this.supabaseService.uploadToPublicStorage(
+        SupabaseBucket.PROFILE_PICTURES,
+        profilePictureFile,
+        id,
+      );
+
+      editProfilePayload.profilePictureUrl = fileUrl;
+    }
+
+    const updatedProfile = await this.prismaService.profiles.update({
+      where: {
+        id,
+      },
+      data: editProfilePayload,
+    });
+
+    return updatedProfile;
+  }
+}
