0ec7466c1f4bcd5b5f931965ef032aa8010b3359
Arif Ramdani <108125940+shelllbyyyyyy@users.noreply.github.com>
2024-02-14 09:40:28 +0700

    fix(build): update compiler

--- apps/client/next.config.mjs
+++ apps/client/next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: [
-    "@shelby/db",
-    "@shelby/supabase",
-    "@shelby/api",
-    "@shelby/dto",
-  ],
   env: {
     SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY,
